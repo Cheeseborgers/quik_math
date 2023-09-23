16861eb6dec716f8f9a13d1ec297cb7afb1ba93f
Goode_Cheeseburgers <goodcheeseburgers@gmail.com>
2023-09-23 17:27:19 +0100

    added vec type concepts for bool, int, floats

--- include/concepts.hpp
+++ include/concepts.hpp
@@ -3,6 +3,10 @@
 
 #include "base.hpp"
 
+// Template meta-variable that checks if a type is the same as 'bool'
+template <typename T>
+constexpr bool IsBooleanT = std::is_same_v<T, bool>;
+
 /**
  * @brief Concept that checks if a type is a floating-point type.
  * @tparam T The type to check.
@@ -11,7 +15,7 @@ template <typename T>
 concept IsFloatingPointT = std::numeric_limits<T>::is_iec559;
 
 /**
- * @brief Concept that checks if a type is a integral type.
+ * @brief Concept that checks if a type is an integral type.
  * @tparam T The type to check.
  */
 template <typename T>
@@ -23,5 +27,38 @@ concept IsIntegerT = std::is_integral_v<T>;
  */
 template <typename T>
 concept IsNumberT = std::is_arithmetic_v<T>;
+
+/**
+ * @brief Concept that checks if a type is a floating-point vector of a specific size.
+ * @tparam T The type to check.
+ * @tparam Size The required size of the vector.
+ */
+template <typename T, std::size_t Size>
+concept IsFloatVec = IsFloatingPointT<T> && std::tuple_size_v<T> == Size;
+
+/**
+ * @brief Concept that checks if a type is an integral vector of a specific size.
+ * @tparam T The type to check.
+ * @tparam Size The required size of the vector.
+ */
+template <typename T, std::size_t Size>
+concept IsIntegerVec = IsIntegerT<T> && std::tuple_size_v<T> == Size;
+
+/**
+ * @brief Concept that checks if a type is a numeric (integer or floating-point) vector of a
+ * specific size.
+ * @tparam T The type to check.
+ * @tparam Size The required size of the vector.
+ */
+template <typename T, std::size_t Size>
+concept IsNumberVec = IsNumberT<T> && std::tuple_size_v<T> == Size;
+
+/**
+ * @brief Concept that checks if a type is a boolean vector of a specific size.
+ * @tparam T The type to check.
+ * @tparam Size The required size of the vector.
+ */
+template <typename T, std::size_t Size>
+concept IsBoolVec = IsBooleanT<T> && std::tuple_size_v<T> == Size;
 
 #endif // QUIKMAFF_CONCEPTS_HPP
